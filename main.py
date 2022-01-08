c23a08a968744e4dfca3bcce0c030f1615006ea1
roshni25vishwakarma <swati250396@gmail.com>
2022-01-08 10:42:37 +0530

    error

--- main.py
+++ main.py
@@ -15,6 +15,16 @@ def introduction():
         ## Computer Vision
         -Computer vision is a field of artificial intelligence (AI) that enables computers and systems to derive meaningful information from digital images, videos and other visual inputs — and take actions or make recommendations based on that information.
     """)
+def introduction():
+    st.markdown("""
+        ## Instructions
+    
+    Use One Finger for Volume Up.
+    Use Two Fingers for Volume Down.
+    Use Three Fingers for Volume Mute.
+    Use Four Fingers for Play.
+    Use Five Fingers for Pause.
+    """)
 
     c1= st.columns(1)
 
